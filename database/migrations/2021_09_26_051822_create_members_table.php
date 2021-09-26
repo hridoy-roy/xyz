5f978086ed8482359289563f2d27995c902da75d
Hridoy Roy <hridayroy05@gmail.com>
2021-09-26 11:26:41 +0600

    Defining Foreign Key Relationship using Laravel Migration 31

---
+++ database/migrations/2021_09_26_051822_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id('member_id');
+            $table->string('name',255);
+            $table->string('email');
+            $table->string('number',11);
+            $table->unsignedBigInteger('mgroup_id');
+            $table->foreign('mgroup_id')->references('group_id')->on('group');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
