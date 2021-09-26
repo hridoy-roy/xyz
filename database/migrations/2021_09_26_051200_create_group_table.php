35d910c37dcc5045db59e774e0e6af8a6bb1e240
Hridoy Roy <hridayroy05@gmail.com>
2021-09-26 11:27:00 +0600

    Defining Foreign Key Relationship using Laravel Migration 31

---
+++ database/migrations/2021_09_26_051200_create_group_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->id('group_id');
+            $table->string('Group_name',255 );
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group');
+    }
+}
